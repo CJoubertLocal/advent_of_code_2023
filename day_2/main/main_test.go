3d825ccdb82914685b6910ecd386f4b5d036beab
CJoubertLocal <63001182+CJoubertLocal@users.noreply.github.com>
2023-12-07 21:52:16 +0000

    Update main_test.go
    Updated for part 2.

--- day_2/main/main_test.go
+++ day_2/main/main_test.go
@@ -108,3 +108,90 @@ func TestColourCountIsGreaterThanMaximum(t *testing.T) {
 		}
 	}
 }
+
+func TestSumOfPowersOfGames(t *testing.T) {
+	type testCase struct {
+		input          []string
+		expectedOutput int
+	}
+
+	testCases := []testCase{
+		{
+			[]string{
+				"Game 1: 0 red",
+				"Game 234: 1 red",
+			},
+			1,
+		},
+		{
+			[]string{
+				"Game 43: 2 red",
+				"Game 564: 2 red, 1 green"},
+			4,
+		},
+		{
+			[]string{
+				"Game 23: 2 red, 1 green, 1 blue",
+				"Game 654: 2 red, 1 green, 0 blue",
+			},
+			2,
+		},
+		{
+			[]string{
+				"Game 6: 20 red, 12 green, 0 blue",
+				"Game 9876: 20 red, 12 green, 0 blue",
+				"Game 73: 2 red; 1 green; 0 blue",
+				"Game 29: 2 red; 1 green, 1  blue; 0 blue",
+				"Game 29: 20 red, 47 green, 13 blue; 100 green, 46 blue; 46 green, 0 blue",
+			},
+			0 + 0 + 0 + 2 + 92000,
+		},
+		{
+			[]string{
+				"Game 76: 7 blue; 15 blue, 2 red, 1 green; 2 blue, 5 red; 2 red, 15 blue; 4 red, 15 blue; 9 blue, 5 red",
+				"Game 77: 12 blue, 8 green, 15 red; 12 blue, 19 red, 16 green; 6 blue, 5 green, 16 red",
+				"Game 78: 2 red, 7 blue, 14 green; 1 red, 3 green, 1 blue; 4 blue, 8 green, 10 red",
+				"Game 79: 3 red, 5 green; 2 blue, 1 red, 18 green; 4 red, 15 green, 2 blue; 18 green, 2 blue, 7 red; 7 green, 6 red",
+				"Game 80: 8 green, 5 red, 9 blue; 14 blue, 13 red, 6 green; 14 blue, 7 red, 4 green; 3 blue, 16 red, 4 green; 5 green, 13 blue, 2 red; 16 blue, 2 green, 5 red",
+			},
+			(5 * 1 * 15) + (19 * 16 * 12) + (10 * 14 * 7) + (7 * 18 * 2) + (16 * 8 * 16),
+		},
+	}
+
+	for i, tst := range testCases {
+		res := sumOfPowersOfGames(tst.input)
+		if res != tst.expectedOutput {
+			t.Errorf("game %d: input %s: expected %d, but got %d",
+				i, tst.input, tst.expectedOutput, res)
+		}
+	}
+}
+
+func TestGetPowerOfGame(t *testing.T) {
+	type testCase struct {
+		input          string
+		expectedOutput int
+	}
+
+	testCases := []testCase{
+		{"Game 1: 0 red", 0},
+		{"Game 234: 1 red", 1},
+		{"Game 43: 2 red", 2},
+		{"Game 564: 2 red, 1 green", 2},
+		{"Game 23: 2 red, 1 green, 1 blue", 2},
+		{"Game 654: 2 red, 1 green, 0 blue", 0},
+		{"Game 6: 20 red, 7851 green, 0 blue", 0},
+		{"Game 9876: 20 red, 7851 green, 0 blue", 0},
+		{"Game 73: 2 red; 1 green; 0 blue", 0},
+		{"Game 29: 2 red; 1 green, 1  blue; 0 blue", 2},
+		{"Game 29: 20 red, 47 green, 13 blue; 100 green, 46 blue; 46 green, 0 blue", 92000},
+	}
+
+	for i, tst := range testCases {
+		res := getPowerOfGame(tst.input)
+		if res != tst.expectedOutput {
+			t.Errorf("game %d: input %s: expected %d, but got %d",
+				i, tst.input, tst.expectedOutput, res)
+		}
+	}
+}
